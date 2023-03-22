4da9b3a01f2c73868b8b6993c01caea8341b2da8
Krish raiturkar <123010893+krishraiturkar@users.noreply.github.com>
2023-03-22 15:27:05 +0530

    read opration

--- result.php
+++ result.php
@@ -14,9 +14,14 @@ else
     echo"mysqli connection sucessful<br>";
 } 
 //targating the database for table and its colume 
-  $result ="SELECT `STUDENT NAME`, `FATHER NAME`, `MOTHER NAME`, `ADMISSON NO`, `ROLL`, `GENDER` FROM `student table` WHERE 1";
-   $showing = mysqli_query($conn,$result);
-   $fetching = mysqli_fetch_assoc($showing);
-   $row = mysqli_num_rows($showing);
-   echo "id:".$showing['0']"-NAMEE".$showing['1']."".$showing['2']."".$showing['3']."".$showing['4']."".$showing['5']."<BR>";
+  $connit ="SELECT `STUDENT NAME`, `FATHER NAME`, `MOTHER NAME`, `ADMISSON NO`, `ROLL`, `GENDER` FROM `student table` WHERE 1";
+   $showing = mysqli_query($conn,$connit);
+   $result = mysqli_num_rows($showing);
+   echo "$result";
+   while($result=$fetching=mysqli_fetch_assoc($showing))
+   {
+        echo "STUDENT NAME:".$fetching['STUDENT NAME']."_FATHER NAME:".$fetching['FATHER NAME'].
+        "_MOTHER NAME:".$fetching['MOTHER NAME']."_ROLL NO:".$fetching['ROLL']."_ADIMISSON NO:"
+        .$fetching['ADMISSON NO']."_GENDER:".$fetching['GENDER']."<BR>";
+   }
 ?>
